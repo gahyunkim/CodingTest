0e5cd534f3b4c1ad3fc0ca5ef0ed86f2f1834dac
gahyunkim <mapledt001@naver.com>
2024-11-07 23:45:18 +0900

    [Gold IV] Title: Yes or yes, Time: 2756 ms, Memory: 55108 KB -BaekjoonHub

--- 백준/Gold/25195. Yes or yes/Yes or yes.py
+++ 백준/Gold/25195. Yes or yes/Yes or yes.py
@@ -1,29 +1,27 @@
-from collections import deque
 n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 s = int(input())
 gom = list(map(int, input().split()))
-visited = [False for _ in range(n+1)]
+visited = [False for _ in range(n + 1)]
 
-def bfs():
-    q = deque()
-    q.append(1)
-    if visited[1]:
-        return 'Yes'
-    visited[1] = True
-    while q:
-        v = q.popleft()
-        if not graph[v]:
-            return 'yes'
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
-    return 'Yes'
-    
 for g in gom:
     visited[g] = True
-print(bfs())
+
+def dfs(v):
+    stack = [v]
+    while stack:
+        node = stack.pop()
+        if visited[node]:
+            return 'Yes'
+        visited[node] = True
+        if not graph[node]:
+            return 'yes'
+        for i in graph[node]:
+            if not visited[i]:
+                stack.append(i)
+    return 'Yes'
+
+print(dfs(1))
